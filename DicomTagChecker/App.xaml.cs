8d2eac785fe708dd3d1cb2634a061578112a89b7
matsumoto-054 <matsumoto@ustec.co.jp>
2020-07-14 18:52:14 +0900

    first commit

---
+++ DicomTagChecker/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using DicomTagChecker.Views;
+using System.Windows;
+
+namespace DicomTagChecker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
