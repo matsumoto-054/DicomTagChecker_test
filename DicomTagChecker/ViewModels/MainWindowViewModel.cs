f965db12c7953e52895434efaeda6c590d7ea635
matsumoto-054 <matsumoto@ustec.co.jp>
2020-07-15 10:19:36 +0900

    タイトル入力

--- DicomTagChecker/ViewModels/MainWindowViewModel.cs
+++ DicomTagChecker/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace DicomTagChecker.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "不正Dicomタグチェッカー";
         public string Title
         {
             get { return _title; }
