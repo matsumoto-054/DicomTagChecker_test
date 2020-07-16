96e30f2e33f53034718909ea16b9525568a0e94e
matsumoto-054 <matsumoto@ustec.co.jp>
2020-07-16 18:17:02 +0900

    ボタンクリックによる仮のイベントを記述

--- DicomTagChecker/ViewModels/MainWindowViewModel.cs
+++ DicomTagChecker/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,43 @@
 ﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Windows.Controls;
 
 namespace DicomTagChecker.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "不正Dicomタグチェッカー";
+
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
+        public ReactiveProperty<string> FolderPath { get; private set; } = new ReactiveProperty<string>(string.Empty);
+
+        public ReactiveCommand<TextBox> SelectFolderButton { get; }
+
+        public ReactiveProperty<string> Log { get; } = new ReactiveProperty<string>();
+
+        public ReactiveCommand StartButton { get; }
+
+        public ReactiveCommand CancelButton { get; }
+
         public MainWindowViewModel()
         {
+            this.FolderPath = new ReactiveProperty<string>("");
+            this.SelectFolderButton = new ReactiveCommand<TextBox>()
+                .WithSubscribe(textBox => this.SelectFolder(textBox));
+        }
 
+        private void SelectFolder(TextBox textBox)
+        {
+            textBox.Text = "test";
+        }
+
+        private void StartReading()
+        {
         }
     }
 }
